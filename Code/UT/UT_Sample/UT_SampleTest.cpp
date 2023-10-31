f8dc523dec849ef7d947007ca9065aca9bb49d68
Arnsu <ferris202@gmail.com>
2023-10-31 08:57:53 +0100

    Basic cmakeSetup

---
+++ Code/UT/UT_Sample/UT_SampleTest.cpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <catch.hpp>
+#include <cstdlib>
+
+namespace CatchTestsetFail
+{
+    TEST_CASE("SampleTest")
+    {
+        CHECK(true);
+    }
+} // End namespace: CatchTestsetFail
---
+++ generateSolution.bat
@@ -0,0 +1,4 @@
+mkdir _buildVS2022
+cd _buildVS2022
+cmake .. -G "Visual Studio 17 2022" -A x64
+pause
