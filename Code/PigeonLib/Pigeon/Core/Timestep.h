b8727fc25f58fb2a5905dc25651973fe5de37b5b
pnkthrepwood <pnkthrepwood@gmail.com>
2024-03-10 22:20:35 +0100

    Add clock class

--- Code/PigeonLib/Pigeon/Core/Timestep.h
+++ Code/PigeonLib/Pigeon/Core/Timestep.h
@@ -7,13 +7,17 @@ namespace pig
 	public:
 		Timestep(uint64_t time = 0)
 			: m_Time(time)
-		{
-		}
+		{}
+
+		Timestep(std::chrono::milliseconds duration) 
+			: m_Time(static_cast<uint64_t>(duration.count()))
+		{}
 
 		operator uint64_t() const { return m_Time; }
 
 		float AsSeconds() const { return m_Time / 1000.f; }
 		uint64_t AsMilliseconds() const { return m_Time; }
+
 	private:
 		uint64_t m_Time;
 	};
