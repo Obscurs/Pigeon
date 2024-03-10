f406ccb6db02580308b03e0ce0652d8f972c63bb
pnkthrepwood <pnkthrepwood@gmail.com>
2024-03-10 22:50:12 +0100

    add Timestep helpers

--- Code/PigeonLib/Pigeon/Core/Timestep.h
+++ Code/PigeonLib/Pigeon/Core/Timestep.h
@@ -5,18 +5,19 @@ namespace pig
 	class Timestep
 	{
 	public:
-		Timestep(uint64_t time = 0)
+		explicit Timestep(uint64_t time = 0) noexcept
 			: m_Time(time)
 		{}
 
-		Timestep(std::chrono::milliseconds duration) 
+		explicit Timestep(std::chrono::milliseconds duration) noexcept
 			: m_Time(static_cast<uint64_t>(duration.count()))
 		{}
 
-		operator uint64_t() const { return m_Time; }
+		operator uint64_t() const noexcept { return m_Time; }
 
-		float AsSeconds() const { return m_Time / 1000.f; }
-		uint64_t AsMilliseconds() const { return m_Time; }
+		float AsMinutes() const noexcept { return m_Time / 1000.f / 60.f; }
+		float AsSeconds() const noexcept { return m_Time / 1000.f; }
+		uint64_t AsMilliseconds() const noexcept { return m_Time; }
 
 	private:
 		uint64_t m_Time;
