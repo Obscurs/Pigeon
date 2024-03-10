cba3eea20f0d104e86c63cdcaa583b4bc9c47c5a
pnkthrepwood <pnkthrepwood@gmail.com>
2024-03-10 18:12:18 +0100

    Manage dependency imgui with FetchContent

--- Code/SandboxApp/SandboxApp.cpp
+++ Code/SandboxApp/SandboxApp.cpp
@@ -3,7 +3,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "imgui/imgui.h"
+#include <imgui.h>
 
 #include "Pigeon/Renderer/OrthographicCamera.h"
 #include "Pigeon/Renderer/Texture.h"
